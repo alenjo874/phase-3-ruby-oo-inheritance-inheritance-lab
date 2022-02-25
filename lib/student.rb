ac4ba4acfa140b082d2991a41649736742b67e9e
Alen Jo <alenjo874@gmail.com>
2022-02-25 10:07:50 -0500

    commit tree af3f7fae221a64430b44797faed2844f4ab3936a

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require "pry"
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(str)
+        self.knowledge << str
+    end
+
+
+
+binding.pry
+    # def knowledge
+       
+    # end
 
 end
